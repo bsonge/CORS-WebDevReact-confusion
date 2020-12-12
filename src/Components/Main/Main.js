f0cf4b9e458588f607df75dc78282dceacdd41c2
bfsonge <bfsonge@gmail.com>
2020-12-12 12:25:50 -0600

    single page applications pt 1: Added Contact component; Adjusted Data Objects in shared (+comments, +promotions, +leaders); added cards to homepage

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -4,7 +4,11 @@ import Menu from '../Menu/Menu.js';
 import Header from '../Header/Header.js';
 import Footer from '../Footer/Footer.js';
 import DishDetail from '../DishDetail/DishDetail.js';
+import Contact from '../Contact/Contact';
 import { DISHES } from '../../shared/dishes.js'
+import { COMMENTS } from '../../shared/comments.js'
+import { LEADERS } from '../../shared/leaders.js'
+import { PROMOTIONS } from '../../shared/promotions.js'
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends React.Component {
@@ -13,6 +17,9 @@ class Main extends React.Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
     };    
   }
 
@@ -21,7 +28,11 @@ class Main extends React.Component {
 
     const HomePage = () => {
       return(
-        <Home />
+        <Home 
+          dish={this.state.dishes.filter(dish => dish.featured)[0]} 
+          promotion={this.state.leaders.filter(promo => promo.featured)[0]} 
+          leader={this.state.promotions.filter(leader => leader.featured)[0]} 
+          />
       )
     } 
 
@@ -31,6 +42,7 @@ class Main extends React.Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
